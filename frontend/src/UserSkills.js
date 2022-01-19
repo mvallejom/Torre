8b1c61088505ead682aec9d8ac5ed51da7ebb3f3
Mateo <m.vallejom1@uniandes.edu.co>
2022-01-19 18:55:52 -0500

    styling

--- frontend/src/UserSkills.js
+++ frontend/src/UserSkills.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import './UserSkills.css'
 class UserSkills extends Component {
 
     state = {
@@ -22,37 +23,57 @@ class UserSkills extends Component {
     render() {
         return (
             <div>
-                <span>Hola</span>
                 <div className='row'>
-                    <div className ="col">
-                    <img src="" alt="Profile"></img>
+                    <div className ="col" id = "user_info">
+                    <img src='https://upload.wikimedia.org/wikipedia/commons/a/a7/Blank_image.jpg' alt="Profile" className="img-thumbnail"></img>
                     <h2 className = "user_name">name</h2>
                     <h4 className ="headline "> headline</h4>
                     <h4 className = "summary"> summary</h4>
                     </div>
-                    <div className ="col">
-                    <h1 className ="user_skills"></h1>
-                    <ul>
-                        <li className = "skills"> Skill1 </li>
-                        <li className = "skills"> Skill2 </li>
-                        <li className = "skills"> Skill3 </li>
-                        <li className = "skills"> Skill4 </li>
-                        <li className = "skills"> Skill5 </li>
-                    </ul>
+                    <div className ="col" id = "user_skills">
+                    <h1 className ="user_skills"> User Skills</h1>
+                    <br></br>
+                    <div class="accordion" id="accordionExample">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingOne">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                                Skill 1
+                            </button>
+                            </h2>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingTwo">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                Skill 2
+                            </button>
+                            </h2>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingThree">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                Skill 3
+                            </button>
+                            </h2>
+                        </div>
+                        </div>
+                    
                     </div>
                 </div>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
                 {this.state.todos.map(item => (
                     <div key={item.id}>
                         <h1>{item.name}</h1>
                         <span>{item.description}</span>
                     </div>
                 ))}
-                <Container>
-                    <Row>
-                        <Col>1</Col>
-                        <Col>2</Col>
-                    </Row>
-                </Container>
+               
             </div>
         );
     }
